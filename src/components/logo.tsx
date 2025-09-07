2ad6781955146ea21c03ad8763ba4e8449322e14
medha Rabhotra <medhasharma476@gmail.com>
2025-09-07 14:58:47 +0000

    now provde the code

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,15 +12,15 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-        <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20z" />
-        <path d="M12 5v2.5" />
-        <path d="M12 16.5V19" />
-        <path d="M16.5 7.5L14.75 9.25" />
-        <path d="M9.25 14.75L7.5 16.5" />
-        <path d="M19 12h-2.5" />
-        <path d="M7.5 12H5" />
-        <path d="M16.5 16.5L14.75 14.75" />
-        <path d="M9.25 9.25L7.5 7.5" />
+      <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20z" />
+      <path d="M12 5v2.5" />
+      <path d="M12 16.5V19" />
+      <path d="M16.5 7.5L14.75 9.25" />
+      <path d="M9.25 14.75L7.5 16.5" />
+      <path d="M19 12h-2.5" />
+      <path d="M7.5 12H5" />
+      <path d="M16.5 16.5L14.75 14.75" />
+      <path d="M9.25 9.25L7.5 7.5" />
     </svg>
   );
 }
